86cda16b0fca08bd30388c78b4044d5a73686293
starsYHyh <2976604815@qq.com>
2024-12-25 00:40:11 +0800

    update dev

--- raft/dstest.sh
+++ raft/dstest.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/zsh
 
-cat test_test.go| grep "2A" | sed 's\(\ \g'|awk '/func/ {printf "%s ",$2;}' | xargs dstest -o .1224_1 -n 5000 -p 50 -r
+cat test_test.go| grep "2B" | sed 's\(\ \g'| awk '/func/ {printf "%s ",$2;}' | xargs dstest -o .1224_2 -n 5000 -p 100 -r
